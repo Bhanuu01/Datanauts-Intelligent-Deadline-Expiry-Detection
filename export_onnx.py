7ad50e4592010383e52b9fb1da5d09e6ca0f2ece
tanish1608 <tanish.vadhineni@gmail.com>
2026-04-20 03:14:30 -0400

    removed tests code

--- export_onnx.py
+++
@@ -1,9 +0,0 @@
-from optimum.onnxruntime import ORTModelForTokenClassification
-from transformers import AutoTokenizer
-
-model_id = "dslim/bert-base-NER"
-tokenizer = AutoTokenizer.from_pretrained(model_id)
-model = ORTModelForTokenClassification.from_pretrained(model_id, export=True)
-
-tokenizer.save_pretrained("./onnx_model")
-model.save_pretrained("./onnx_model")
